5129d5140d83d1829bc511c33e61dad79a169e7b
rrcgat <rrcgat@gmail.com>
2020-11-23 16:43:20 +0800

    Make diary database path configurable

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 )
 
+func expand(path string) string {
+	if strings.HasPrefix(path, "~/") {
+		curr, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+			return path
+		}
+		return filepath.Join(curr.HomeDir, path[2:])
+	}
+	return path
+}
+
 func main() {
-	d, err := NewClient("diary.db", diaryBucket)
+	d, err := NewClient(expand(os.Getenv("DAIRY_DATABASE")), diaryBucket)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
